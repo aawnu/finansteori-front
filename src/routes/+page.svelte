dec161075db94e0601f613b7bd324e32db530b96
AAW <aaw@aaw.nu>
2024-07-07 15:28:05 +0200

    fix: update frontend and fix trailing slashes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <svelte:head>
 	<title>Finansteori</title>
-	<meta
-		name="description"
-		content="Tag kontrol over din økonomi, før den tager kontrol over dig."
-	/>
+	<meta name="description" content="Tag kontrol over din økonomi, før den tager kontrol over dig." />
 </svelte:head>
 
-<div class="flex min-h-[50vh] min-w-full flex-col items-center justify-end">
+<div class="flex min-h-[45vh] min-w-full flex-col items-center justify-end">
 	<h1 class="font-yrsa text-5xl font-black text-finansteori-primary">
 		<a href="/">Finansteori</a>
 	</h1>
 	<p class="text-lg italic">Tag kontrol over din økonomi, før den tager kontrol over dig.</p>
+</div>
+<div class="container mx-auto mt-8 max-w-screen-md border-t border-t-slate-300 text-center">
+	<a href="/platform" class="block p-4 text-blue-600">
+		<b>Sammenlign:</b>
+		Aktiemægler platforme i danmark
+	</a>
 </div>
