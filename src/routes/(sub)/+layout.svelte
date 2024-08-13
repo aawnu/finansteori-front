8bf77f1fb2b20bd10fbb7a2cb7a07ea33d64b5dc
AAW <aaw@aaw.nu>
2024-08-13 03:57:30 +0200

    Update Finansteori logo

--- src/routes/(sub)/+layout.svelte
+++ src/routes/(sub)/+layout.svelte
@@ -1,8 +1,12 @@
+<script>
+	import Finansteori from '$lib/icons/finansteori.svelte'
+</script>
+
 <header>
 	<div class="container mx-auto">
 		<div class="p-4">
 			<h1 class="font-yrsa text-xl font-medium">
-				<a href="/">Finansteori</a>
+				<a href="/"><Finansteori class="h-12" type="logo" /></a>
 			</h1>
 		</div>
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Finansteori from '$lib/icons/finansteori.svelte'
+</script>
+
 <svelte:head>
 	<title>Finansteori</title>
 	<meta name="description" content="Tag kontrol over din økonomi, før den tager kontrol over dig." />
@@ -5,7 +9,7 @@
 
 <div class="flex min-h-[45vh] min-w-full flex-col items-center justify-end">
 	<h1 class="font-yrsa text-5xl font-black text-finansteori-primary">
-		<a href="/">Finansteori</a>
+		<a href="/"><Finansteori class="h-24" type="logo" /></a>
 	</h1>
 	<p class="text-lg italic">Tag kontrol over din økonomi, før den tager kontrol over dig.</p>
 </div>
