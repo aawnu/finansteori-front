94eeb4c481b258ca2af1079f6d89036a9475874a
AAW <aaw@aaw.nu>
2024-08-24 03:40:14 +0200

    Update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <div class="min-h-[calc(100vh-4rem)]">
 	<header>
-		<div class="container flex items-center justify-center md:justify-start py-4">
+		<div class="container flex items-center justify-center py-8 md:justify-start">
 			<a href="/">
 				<Finansteori class="h-16" original />
 			</a>
@@ -16,5 +16,10 @@
 </div>
 
 <footer>
-	<div class="container py-2">2024 &copy; Finansteori</div>
+	<div class="container mt-6 flex items-center justify-between border-t-2 border-t-slate-200 py-6">
+		<div>2024 &copy; Finansteori</div>
+		<div>
+			<a class="text-primary hover:text-primary-300" href="/ansvar">Ansvarsfraskrivelse</a>
+		</div>
+	</div>
 </footer>
