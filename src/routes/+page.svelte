745a8c0ef20cb56fb2fbd06b01410c14ed413834
AAW <aaw@aaw.nu>
2024-11-19 16:13:05 +0100

    Update frontpage header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,20 +23,14 @@
 	<meta property="og:type" content="website" />
 </svelte:head>
 
-<div class="container flex min-h-[45vh] items-center py-12">
-	<div class="space-y-2">
-		<h1
-			class="max-w-screen-md text-center font-lora text-4xl font-bold !leading-tight md:text-left md:text-6xl"
-		>
-			Kontroller din økonomi, før den kontrollere dig.
-		</h1>
-		<p
-			class="max-w-screen-sm text-center text-lg font-light !leading-relaxed md:text-left md:text-2xl"
-		>
-			22% af unge danskere mellem 18 og 35 har problemer med deres økonomi ifølge Finans Danmark i
-			2023.
-		</p>
-	</div>
+<div class="container flex min-h-[45vh] flex-col items-center justify-center space-y-2 py-24">
+	<h1 class="text-center font-lora text-5xl font-black !leading-tight md:text-7xl">
+		<span class="inline lg:block">Kontroller <u>din</u> økonomi,</span>
+		<span class="inline lg:block">før <i>den</i> kontrollere dig.</span>
+	</h1>
+	<p class="max-w-screen-sm text-center text-lg font-light !leading-relaxed md:text-3xl">
+		Har du styr på din økonomi og hvordan du formidler den imod en bedre fremtid?
+	</p>
 </div>
 
 <div class="container space-y-4 py-4 text-xl md:text-2xl">
